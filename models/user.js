f670335d38976e8fe08108a691084bdffeeb6c9a
KIMHYEONGJIN5925 <93527060+KIMHYEONGJIN5925@users.noreply.github.com>
2022-01-07 00:12:56 +0900

    [형진] <chore> model/user.js 수정

--- models/user.js
+++ models/user.js
@@ -1,70 +1,72 @@
+const Sequelize = require("sequelize");
 
-  static init(sequelize) {
-    return super.init(
-      {
-        userId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        kakaoId: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          allowNull: false,
-          defaultValue: 0,
-        },
-        userEmail: {
-          type: Sequelize.STRING(45),
-          allowNull: false,
-          unique: true,
-        },
-        userPassword: {
-          type: Sequelize.STRING(100),
-          allowNull: true,
-        },
-        userNickname: {
-          type: Sequelize.STRING(45),
-          allowNull: false,
-        },
-        userImgUrl: {
-          type: Sequelize.STRING(500),
-          allowNull: true,
-        },
-        userFrag: {
-          type: Sequelize.STRING(45),
-          allowNull: true,
-        },
-        followingCnt: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          allowNull: false,
-          defaultValue: 0,
-        },
-        followerCnt: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          allowNull: false,
-          defaultValue: 0,
-        },
-        likePerfumeCnt: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          allowNull: false,
-          defaultValue: 0,
-        },
-        userReviewCnt: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          allowNull: false,
-          defaultValue: 0,
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.literal("now()"),
-        },
-        updatedAt: {
-          type: Sequelize.DATE,
-          allowNull: true,
-        },
-      },
-      {
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                userId: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                },
+                kakaoId: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                userEmail: {
+                    type: Sequelize.STRING(45),
+                    allowNull: false,
+                    unique: true,
+                },
+                userPassword: {
+                    type: Sequelize.STRING(100),
+                    allowNull: true,
+                },
+                userNickname: {
+                    type: Sequelize.STRING(45),
+                    allowNull: false,
+                },
+                userImgUrl: {
+                    type: Sequelize.STRING(500),
+                    allowNull: true,
+                },
+                userFrag: {
+                    type: Sequelize.STRING(45),
+                    allowNull: true,
+                },
+                followingCnt: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                followerCnt: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                likePerfumeCnt: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                userReviewCnt: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                createdAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal("now()"),
+                },
+                updatedAt: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                },
+            },
+            {
                 sequelize,
                 timestamps: false,
                 underscored: false,
@@ -73,9 +75,9 @@
                 paranoid: false,
                 charset: "utf8mb4",
                 collate: "utf8mb4_general_ci",
-      }
-    );
-  }
+            }
+        );
+    }
     static associate(db) {
         db.User.hasMany(db.Review, {
             foreignKey: "userId",
@@ -84,17 +86,16 @@
         // db.User.belongsToMany(db.Perfume, { through: "perfumeLikes" });
         // db.User.belongsToMany(db.Review, { through: "reviewLikes" });
     }
-  static associate(db) {
-
-    db.User.belongsToMany(db.User, {
-      foreignKey: 'followingId',
-      as: 'Followers',
-      through: 'Follow',
-    });
-    db.User.belongsToMany(db.User, {
-      foreignKey: 'followerId',
-      as: 'Followings',
-      through: 'Follow',
-    });
-  }
+    static associate(db) {
+        db.User.belongsToMany(db.User, {
+            foreignKey: "followingId",
+            as: "Followers",
+            through: "Follow",
+        });
+        db.User.belongsToMany(db.User, {
+            foreignKey: "followerId",
+            as: "Followings",
+            through: "Follow",
+        });
+    }
 };
