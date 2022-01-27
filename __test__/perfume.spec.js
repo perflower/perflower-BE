98436fab56ec13ae4aa507cd848d9bc512c7b5a8
IsthisLee <rjsgmldnwn@gmail.com>
2022-01-27 23:21:00 +0900

    [건희] <chore>

--- __test__/perfume.spec.js
+++ __test__/perfume.spec.js
@@ -1,2 +1,8 @@
 const app = require("../app");
 const request = require("supertest");
+
+describe("GET /", () => {
+  it("Hello, Kangaroo라는 응답이 와야한다.", () => {
+    request(app).get("/").expect("Hello, Kangaroo");
+  });
+});
--- test/mock-test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../app");
-
-describe("GET /", () => {
-  it("Hello, Kangaroo라는 응답이 와야한다.", () => {
-    request(app).get("/").expect("Hello, Kangaroo");
-  });
-});
